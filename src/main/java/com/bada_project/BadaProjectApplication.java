c28429fc1a6f26b7b5cddbbd5a5d0c50f3a34936
cmapella <mapella.corentin@gmail.com>
2017-03-17 09:48:13 +0100

    Création du BadaProject

---
+++ src/main/java/com/bada_project/BadaProjectApplication.java
@@ -0,0 +1,12 @@
+package com.bada_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BadaProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BadaProjectApplication.class, args);
+	}
+}
