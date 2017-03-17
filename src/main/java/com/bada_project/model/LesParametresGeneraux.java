1aa2811c352bf71b7feb7f985789acb80a8e0e1b
Sarahprs <sarah.pierson94@gmail.com>
2017-03-17 11:38:44 +0100

    [MODEL] Create 'ParametresGeneraux'

---
+++ src/main/java/com/bada_project/model/LesParametresGeneraux.java
@@ -0,0 +1,15 @@
+package com.bada_project.model;
+
+import java.util.Date;
+
+public class LesParametresGeneraux {
+	private String Nom;
+	private int HeureDebut;
+	private int HeureFin;
+	private int GrainTemporel;
+	private Date DateDebut;
+	private Date Datefin;
+	private String MatiereV2;
+	private LesConges LesConges;
+
+}
---
+++ src/main/java/com/bada_project/model/UneJournee.java
@@ -0,0 +1,18 @@
+package com.bada_project.model;
+
+import java.util.Date;
+
+public class UneJournee {
+	private Date Date;
+
+	public Date getDate() {
+		return Date;
+	}
+
+	public void setDate(Date date) {
+		Date = date;
+	}
+	
+	
+	
+}
