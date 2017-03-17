97d2970e4a9f50b174adcd43452b79cb40f1aac3
Sarahprs <sarah.pierson94@gmail.com>
2017-03-17 13:47:00 +0100

    [MODEL] Create 'ParametresGenerauxfin '

---
+++ src/main/java/com/bada_project/model/Conge.java
@@ -0,0 +1,21 @@
+package com.bada_project.model;
+
+public class Conge {
+	private UneJournee Journee;
+	private UnePeriode Periode;
+	
+	public UneJournee getJournee() {
+		return Journee;
+	}
+	public void setJournee(UneJournee journee) {
+		Journee = journee;
+	}
+	public UnePeriode getPeriode() {
+		return Periode;
+	}
+	public void setPeriode(UnePeriode periode) {
+		Periode = periode;
+	}
+	
+
+}
