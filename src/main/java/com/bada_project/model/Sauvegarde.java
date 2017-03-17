2648f6febf7b0d3ef310e10e33d568e18973bf54
gtandu <tbrandon91@hotmail.fr>
2017-03-17 11:12:36 +0100

    Classe Sauvegarde

--- src/main/java/com/bada_project/model/Sauvegarde.java
+++ src/main/java/com/bada_project/model/Sauvegarde.java
@@ -1,5 +1,32 @@
 package com.bada_project.model;
 
+import java.util.Date;
+
 public class Sauvegarde {
+	
+	private Date date;
+	private Date version;
+	private String utilisateur;
+	
+	public Date getDate() {
+		return date;
+	}
+	public void setDate(Date date) {
+		this.date = date;
+	}
+	public Date getVersion() {
+		return version;
+	}
+	public void setVersion(Date version) {
+		this.version = version;
+	}
+	public String getUtilisateur() {
+		return utilisateur;
+	}
+	public void setUtilisateur(String utilisateur) {
+		this.utilisateur = utilisateur;
+	}
+	
+	
 
 }
