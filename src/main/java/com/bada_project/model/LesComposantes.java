dadf8a969d05358ec2d935abcbd93c2a4df18dcf
Sarahprs <sarah.pierson94@gmail.com>
2017-03-24 11:14:05 +0100

    [MODEL]Classe  Composante

---
+++ src/main/java/com/bada_project/model/LesComposantes.java
@@ -0,0 +1,17 @@
+package com.bada_project.model;
+
+import java.util.Set;
+
+public class LesComposantes {
+	
+	private Set<Composante> une_composante;
+
+	public Set<Composante> getUn_grade() {
+		return une_composante;
+	}
+
+	public void setUne_Composante(Set<Composante> une_composante) {
+		this.une_composante = une_composante;
+	}
+
+}
