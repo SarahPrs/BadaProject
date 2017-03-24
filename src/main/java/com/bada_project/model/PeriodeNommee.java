b179cbe6fbeedb8fba4338d49dbf60ee6b6e4919
Sarahprs <sarah.pierson94@gmail.com>
2017-03-24 11:30:23 +0100

    [MODEL]Classe  preiodesnommee

---
+++ src/main/java/com/bada_project/model/PeriodeNommee.java
@@ -0,0 +1,21 @@
+package com.bada_project.model;
+
+public class PeriodeNommee {
+	private String un_nom_periode;
+	private UnePeriode des_dates_periode;
+	public String getUn_nom_periode() {
+		return un_nom_periode;
+	}
+	public void setUn_nom_periode(String un_nom_periode) {
+		this.un_nom_periode = un_nom_periode;
+	}
+	public UnePeriode getDes_dates_periode() {
+		return des_dates_periode;
+	}
+	public void setDes_dates_periode(UnePeriode des_dates_periode) {
+		this.des_dates_periode = des_dates_periode;
+	}
+	
+	
+
+}
