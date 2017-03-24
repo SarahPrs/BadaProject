419e05bf5cfb7f1e709aa897acff4cbfe34f9466
lucieseghini <lucieseurin.pro@gmail.com>
2017-03-24 10:59:24 +0100

    [MODEL] Create 'LesProfesseurs'+'LesReservations'

---
+++ src/main/java/com/bada_project/model/LesProfesseurs.java
@@ -0,0 +1,17 @@
+package com.bada_project.model;
+
+import java.util.Set;
+
+public class LesProfesseurs {
+
+	private Set<Professeur> professeur;
+
+	public Set<Professeur> getProfesseur() {
+		return professeur;
+	}
+
+	public void setProfesseur(Set<Professeur> professeur) {
+		this.professeur = professeur;
+	}
+	
+}
--- src/main/java/com/bada_project/model/Professeur.java
+++ src/main/java/com/bada_project/model/Professeur.java
@@ -2,4 +2,125 @@ package com.bada_project.model;
 
 public class Professeur {
 
+	private float code;
+	private float dateModification;
+	private String nom;
+	private String prenom;
+	private String prenom2;
+	private String alias;
+	private String identificationNational;
+	private String identifiant;
+	private String commentaire;
+	private int composante;
+	private String dateDossier;
+	private String naissance;
+	private Cnu cnu;
+	private Grade grade;
+	private float codeProprietaire;
+	private int couleur;
+	private int couleurPolice;
+	
+	public float getCode() {
+		return code;
+	}
+	public void setCode(float code) {
+		this.code = code;
+	}
+	public float getDateModification() {
+		return dateModification;
+	}
+	public void setDateModification(float dateModification) {
+		this.dateModification = dateModification;
+	}
+	public String getNom() {
+		return nom;
+	}
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	public String getPrenom() {
+		return prenom;
+	}
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+	public String getPrenom2() {
+		return prenom2;
+	}
+	public void setPrenom2(String prenom2) {
+		this.prenom2 = prenom2;
+	}
+	public String getAlias() {
+		return alias;
+	}
+	public void setAlias(String alias) {
+		this.alias = alias;
+	}
+	public String getIdentificationNational() {
+		return identificationNational;
+	}
+	public void setIdentificationNational(String identificationNational) {
+		this.identificationNational = identificationNational;
+	}
+	public String getIdentifiant() {
+		return identifiant;
+	}
+	public void setIdentifiant(String identifiant) {
+		this.identifiant = identifiant;
+	}
+	public String getCommentaire() {
+		return commentaire;
+	}
+	public void setCommentaire(String commentaire) {
+		this.commentaire = commentaire;
+	}
+	public int getComposante() {
+		return composante;
+	}
+	public void setComposante(int composante) {
+		this.composante = composante;
+	}
+	public String getDateDossier() {
+		return dateDossier;
+	}
+	public void setDateDossier(String dateDossier) {
+		this.dateDossier = dateDossier;
+	}
+	public String getNaissance() {
+		return naissance;
+	}
+	public void setNaissance(String naissance) {
+		this.naissance = naissance;
+	}
+	public Cnu getCnu() {
+		return cnu;
+	}
+	public void setCnu(Cnu cnu) {
+		this.cnu = cnu;
+	}
+	public Grade getGrade() {
+		return grade;
+	}
+	public void setGrade(Grade grade) {
+		this.grade = grade;
+	}
+	public float getCodeProprietaire() {
+		return codeProprietaire;
+	}
+	public void setCodeProprietaire(float codeProprietaire) {
+		this.codeProprietaire = codeProprietaire;
+	}
+	public int getCouleur() {
+		return couleur;
+	}
+	public void setCouleur(int couleur) {
+		this.couleur = couleur;
+	}
+	public int getCouleurPolice() {
+		return couleurPolice;
+	}
+	public void setCouleurPolice(int couleurPolice) {
+		this.couleurPolice = couleurPolice;
+	}
+	
 }
