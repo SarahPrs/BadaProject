8ba0cdc13247fcfa2f735f4bf98ebafa3c68a25b
Sarahprs <sarah.pierson94@gmail.com>
2017-03-24 10:37:13 +0100

    [MODEL]Classe  Matiere

---
+++ src/main/java/com/bada_project/model/LesMatieres.java
@@ -0,0 +1,17 @@
+package com.bada_project.model;
+
+import java.util.Set;
+
+public class LesMatieres {
+	
+	private Set<Matiere> une_matiere;
+
+	public Set<Matiere> getUn_grade() {
+		return une_matiere;
+	}
+
+	public void setUne_Matiere(Set<Matiere> une_matiere) {
+		this.une_matiere = une_matiere;
+	}
+
+}
