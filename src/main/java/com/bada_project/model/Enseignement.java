dc36304e9374adf994716237903f896daf6f455f
Sarahprs <sarah.pierson94@gmail.com>
2017-04-05 16:06:59 +0200

    enseingnement

---
+++ src/main/java/com/bada_project/model/Enseignement.java
@@ -0,0 +1,27 @@
+package com.bada_project.model;
+
+public class Enseignement {
+	private int code;
+	private String nom;
+	private int type_activite;
+	
+	public int getCode() {
+		return code;
+	}
+	public void setCode(int code) {
+		this.code = code;
+	}
+	public String getNom() {
+		return nom;
+	}
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	public int getType_activite() {
+		return type_activite;
+	}
+	public void setType_activite(int type_activite) {
+		this.type_activite = type_activite;
+	}
+	
+}
---
+++ src/main/java/com/bada_project/model/LesEnseignements.java
@@ -0,0 +1,17 @@
+package com.bada_project.model;
+
+import java.util.*;
+
+public class LesEnseignements {
+	
+	private ArrayList<Enseignement> un_enseignement;
+
+	public ArrayList<Enseignement> getUn_Enseignement() {
+		return un_enseignement;
+	}
+
+	public void setUne_Matiere(ArrayList<Enseignement> un_enseignement) {
+		this.un_enseignement = un_enseignement;
+	}
+
+}
